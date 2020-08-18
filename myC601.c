5e4fbd71ed2bc8339505f861b77e5c689df6673e
parichrt <69183300+parichrt@users.noreply.github.com>
2020-08-18 11:15:57 +0700

    ส่งจากปาริชาติ

---
+++ myC601.c
@@ -0,0 +1,26 @@
+/*Program Name : program to Start
+Author : Mrs.Parichart Chansida
+Dete : 26/82001
+*/
+#include<stdio.h>//link from library stdio.h
+void message();//function prototype declartion section
+void cat();//function prototype declartion section
+
+void main(){
+    int animal=5;//variable declaration
+    printf("Hello World\n");
+    printf("%i",amimal);
+    message();
+    cat();
+}
+
+//Sub function : message
+void message(){
+    printf("We can take mor sub function\n");
+}
+
+//Sub function : cat
+void cat(){
+    printf("I love My Cat\n");
+}
+
---
+++ myC602.c
@@ -0,0 +1,15 @@
+/*
+Program c-basic Syntax
+Author : Mrs.Parichart Chansida
+Dete : 8/18/2563
+*/
+#include<stdio.h>//header files 
+
+void main(){//Main function
+   int a=5; //Token==>keyword == varia
+   int b=10;
+   int c=20;
+   int total = a+b+c;
+   printf("1%/n",total);
+
+}
